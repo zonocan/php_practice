51445127cf47007a998c21245819aaa877bc7f4e
zonocan <zonocan.y.m@icloud.com>
2022-02-05 04:45:49 +0000

    間違い箇所の訂正

--- practice.php
+++ practice.php
@@ -1,26 +1,26 @@
 <?php
- echo 'hello php!';
+ echo 'hello php!'."\n";
 
 //課題1
  $a = 3;
  $b = 7;
- echo $a+$b;
+ echo $a+$b."\n";
 
 //課題2
 $array_month = array("1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月");
-echo $array_month[7];
+echo $array_month[7]."\n";
 
 //課題3
 $hello = "Hello";
 $name = "yutaro";
 $world = "'s World!";
 
-echo $hello,$name,$world;
+echo $hello,$name,$world."\n";
 
 //課題4
 $tech_boost = "tech";
 $tech_boost .= "boost";
-echo $tech_boost;
+echo $tech_boost."\n";
 
 //課題5
 $calendar = array(
@@ -38,35 +38,40 @@ $calendar = array(
     "December" => "12月"
     );
     
-    echo $calendar["December"];
+    echo $calendar["December"]."\n";
  
  
  //条件分と繰り返し文
  
  //課題1
- $name = "雄太郎";
- 
-    if($name == "雄太郎"){
-        echo "私はあなたの名前です";
-    }else {
-        echo "あなたの名前ではありません";
+    $name = "雄太郎";
+    if ($name === "雄太郎") {
+        echo "私は" . $name . "です"."\n";
+    } else {
+        echo $name . "ではありません ";
     }
-    
+
  //課題2
-     for($i = 1; $i <= 10000; $i++ ){
-        echo $i."\n";
+    $start = 1;
+    $end = 10000;
+    $total = 0;
+    for($i = $start; $i <= $end; $i++ ){
+        $total += $i;
     }
+    echo $total."\n";
     
+   
  //課題3
-     $fruits = array("apple","greap","lemon","mango","painaple");
+     $fruits = array ("apple","greap","lemon","mango","painaple");
     
-    foreach ($fruits as $fruit){
+    foreach ($fruits as $fruit) {
         echo $fruit."\n";
     }
 
- //課題3
+ //課題4
      for ($i = 1; $i <= 100; $i++) {
         if ($i % 5 == 0) {
             echo $i."\n";
         }
     }
+    
